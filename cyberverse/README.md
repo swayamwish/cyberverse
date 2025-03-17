c270b29f205de4fd4074d8304640eb9ab21a2770
swayamwish <swayamwish@gmail.com>
2025-03-17 14:23:46 +0530

    Add files via upload

---
+++ cyberverse/README.md
@@ -0,0 +1,3 @@
+# Build and Deploy a Modern Next 13 Website With Framer Motion & Tailwind CSS
+
+### [Live Site] link = https://cyberverse-sk.vercel.app/
---
+++ cyberverse/constants/index.js
@@ -0,0 +1,82 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-03.png',
+    title: 'MetaHorizon',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-05.png',
+    title: 'InfinityVerse',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-04.png',
+    title: 'HyperVerse',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-02.png',
+    title: 'Quantum Nexus',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-01.png',
+    title: 'Pandora',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-08.png',
+    title: 'The launch of the Cyberverse leaves Elon Musk uneasy.',
+    subtitle:
+        'Great times also come with challenges, but innovation continues to thrive. Significant changes bring new opportunities and excitement.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Cyberverse',
+    subtitle:
+        'Life is a journey of learning and adaptation. Challenges shape us, and every step forward brings new opportunities for growth.',
+  },
+  {
+    imgUrl: '/planet-06.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Each individual adapts to challenges with strength. True growth comes from learning, perseverance, and embracing new opportunities.',
+  },
+];
+export const socials = [
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/sumit-kumar176/'
+  },
+  {
+    name: 'github',
+    url: '/github.svg',
+    link: 'https://github.com/skipper-108/'
+  },
+  
+];
